908b59503c291d854fee053c96066c9374417a99
Alejandro Sanchez <sanchez.alejandro0603@gmail.com>
2021-03-06 12:49:41 -0600

    puntos 1 y 3 postwork

---
+++ postworkbd.sql
@@ -0,0 +1,37 @@
+INSERT INTO `biblioteca_digital`.`bibliotecario` (`idBibliotecario`, `password`, `status`) VALUES ('15080710', 'tomatesverdes', 'activo');
+INSERT INTO `biblioteca_digital`.`bibliotecario` (`idBibliotecario`, `password`, `status`) VALUES ('15080720', 'frito22', 'activo');
+INSERT INTO `biblioteca_digital`.`bibliotecario` (`idBibliotecario`, `password`, `status`) VALUES ('15080730', 'password1', 'activo');
+INSERT INTO `biblioteca_digital`.`bibliotecario` (`idBibliotecario`, `password`, `status`) VALUES ('15080740', '12345678', 'activo');
+INSERT INTO `biblioteca_digital`.`bibliotecario` (`idBibliotecario`, `password`, `status`) VALUES ('15080750', 'elmejor09', 'inactivo');
+
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('978-84-7888-628-9', 'el principito', 'fabula', 'salamandra s.a.', '111', '06-04-1943', 'español');
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('9788417047146', 'el gallo de oro y otros relatos', 'drama', 'rm verlag s.l.', '203', '01-04-2017', 'español');
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('9788493442606', 'pedro páramo', 'drama', 'rm verlag s.l.', '132', '01-11-2005', 'español');
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('9788493442613', 'el llano en llamas', 'drama', 'rm verlag s.l.', '169', '01-12-2005', 'español');
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('9788498016284', 'la vuelta al mundo en 80 días', 'ciencia-ficción', 'art blume', '243', '30-01-1873', 'inglés');
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('9788408215103', 'de la tierra a la luna', 'ciencia-ficción', 'art blume', '322', '25-10-1863', 'inglés');
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('9788494852060', '20,000 leguas de viaje submarino', 'ciencia-ficción', 'art blume', '368', '20-06-1870', 'inglés');
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('9788427213548', 'viaje al centro de la tierra', 'ciencia-ficción', 'art blume', '248', '25-11-1864', 'inglés');
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('9788427214927', 'estudio en escarlata', 'misterio', 'rba molino', '131', '18-11-1887', 'español');
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('9788415618775', 'el sabueso de los baskerville', 'misterio', 'rba molino', '194', '14-04-1902', 'español');
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('9788446045212', 'el signo de los cuatro', 'misterio', 'rba molino', '162', '10-02-1890', 'español');
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('978-8467861563', 'el valle del terror', 'misterio', 'rba molino', '252', '27-02-1915', 'español');
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('978-8416859306', 'eragon', 'fantasía', 'rocabolsillo', '656', '12-06-2009', 'español');
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('9788497592451', 'el amor en los tiempos del cólera', 'realismo mágico', 'debolsillo', '496', '01-01-1985', 'español');
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('9786070729515', 'la mala hora', 'realismo mágico', 'debolsillo', '192', '01-01-1982', 'español');
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('9788466331906', 'rayuela', 'novela', 'debolsillo', '736', '28-06-1963', 'español');
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('9788466331890', 'historias de cronopios y de famas', 'novela', 'debolsillo', '160', '01-01-1965', 'español');
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('9788437638973', 'la ciudad y los perros', 'novela', 'catedra', '744', '04-10-1963', 'español');
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('9788490626726', 'la tregua', 'novela', 'debolsillo', '208', '01-01-1960', 'español');
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('9788467055177', 'hojas de hierba', 'poesía', 'S.L.U. ESPASA LIBROS', '288', '04-07-1855', 'inglés');
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('9788439730132', 'el conde de montecristo', 'novela', 'literatura random', '1144', '04-06-1844', 'francés');
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('9788491052401', 'los tres mosqueteros', 'novela', 'literatura random', '738', '20-10-1844', 'francés');
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('9788445077535', 'el silmarillion', 'fantasía', 'minotauro', '448', '01-01-1901', 'inglés');
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('9788445006498', 'los hijos de hurin', 'fantasía', 'minotauro', '304', '16-04-2007', 'inglés');
+INSERT INTO `biblioteca_digital`.`libro` (`ISBN`, `titulo`, `genero`, `editorial`, `paginas`, `publicacion`, `idioma`) VALUES ('9788445072974', 'roverandom', 'fantasía', 'minotauro', '118', '20-06-1998', 'inglés');
+
+INSERT INTO `biblioteca_digital`.`miembro` (`idMiembro`, `password`, `status`) VALUES ('200110', 'tomatesrojos', 'activo');
+INSERT INTO `biblioteca_digital`.`miembro` (`idMiembro`, `password`, `status`) VALUES ('200120', 'cuadradocircular', 'suspendido');
+
+INSERT INTO `biblioteca_digital`.`catalogo` (`idCatalogo`, `idMiembro`, `cantidadLibros`, `tituloLibro`, `autoresLibros`, `genero`, `fechasPublicacion`) VALUES ('10501', '200110', '2', 'el principito, el silmarillion', 'antoine de saint-exupéry, j.r.r. tolkien', 'fabula, fantasía', '06-04-1943, 01-01-1901');
+INSERT INTO `biblioteca_digital`.`catalogo` (`idCatalogo`, `idMiembro`, `cantidadLibros`, `tituloLibro`, `autoresLibros`, `genero`, `fechasPublicacion`) VALUES ('10502', '200120', '3', 'el conde de montecristo, hojas de hierba, estudio en escarlata', 'alexandre dumas, walt whitman, arthur conan doyle', 'novela, poesía, misterio', '04-06-1844, 04-07-1855, 18-11-1887');
